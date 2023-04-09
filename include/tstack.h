4a0a9002a0fdc9a365b116fb88eb995a6f738b96
SupeRWiNdz <125024771+SupeRWiNdz@users.noreply.github.com>
2023-04-09 16:33:12 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,38 +4,36 @@
 #include <string>
 
 template<typename T, int size>
-class TStack
-{
-    private:
-        T arr[100];
-        int top;
+class TStack {
+ private:
+    T arr[100];
+    int top;
 
-    public:
-        TStack() : top(-1) {}
-        void push(T value) {
-            if (isFull())
-                throw std::string("Full");
-            else
-                arr[++top] = value;
-        }
-        const T& pop() {
-            if (isEmpty())
-                throw std::string("empty");
-            else
-                return arr[top--];
-        }
-        const T& getTop() const {
-           if (isEmpty())
-               throw std::string("Empty!");
-           else
-               return arr[top];
-           }
-        bool isEmpty() const {
-            return top == -1;
-        }
-        bool isFull() const {
-            return top == size - 1;
-        }
+ public:
+    TStack() : top(-1) {}
+    void push(T value) {
+    if (isFull())
+        throw std::string("Full");
+    else
+        arr[++top] = value;
+    }
+    const T& pop() {
+        if (isEmpty())
+            throw std::string("empty");
+        else
+            return arr[top--];
+    }
+    const T& getTop() const {
+    if (isEmpty())
+        throw std::string("Empty!");
+    else
+        return arr[top];
+    bool isEmpty() const {
+        return top == -1;
+    }
+    bool isFull() const {
+    return top == size - 1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
