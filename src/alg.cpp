fe0398ab3ba32961fc82ac7857a8e3b7736bd7e8
SupeRWiNdz <125024771+SupeRWiNdz@users.noreply.github.com>
2023-04-09 16:02:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,15 +14,13 @@ std::string infx2pstfx(std::string inf) {
     TStack<char, 100> stack1;
     std::string output = "";
     for (int i = 0; i < inf.length(); i++) {
-        if (inf[i] >= '0' && inf[i] <= '9')
-        {
+        if (inf[i] >= '0' && inf[i] <= '9') {
             output += inf[i];
             output += " ";
         }
         else if (inf[i] == '(')
             stack1.push(inf[i]);
-        else if (inf[i] == '-' || inf[i] == '+' || inf[i] == '/' || inf[i] == '*')
-        {
+        else if (inf[i] == '-' || inf[i] == '+' || inf[i] == '/' || inf[i] == '*') {
             while (stack1.isEmpty() == 0 && getPrior(inf[i]) <= getPrior(stack1.getTop()))
             {
                 output += stack1.getTop();
@@ -40,7 +38,6 @@ std::string infx2pstfx(std::string inf) {
             }
             stack1.pop();
         }
-
     }
     while (stack1.isEmpty()==0) {
         output += stack1.getTop();
@@ -52,28 +49,23 @@ std::string infx2pstfx(std::string inf) {
 int eval(std::string post) {
     TStack<int, 100> stack2;
     for (int i = 0; i < post.length(); i++) {
-        if (post[i] == ' ') {
+        if (post[i] == ' ')
             continue;
-        }
-        if (post[i] >= '0' && post[i] <= '9') {
+        if (post[i] >= '0' && post[i] <= '9')
             stack2.push(post[i] - '0');
-        }
         else if (post[i] == '-' || post[i] == '+' || post[i] == '/' || post[i] == '*') {
             int temp1 = stack2.getTop();
             stack2.pop();
             int temp2 = stack2.getTop();
             stack2.pop();
-            if (post[i] == '+') {
+            if (post[i] == '+')
                 stack2.push(temp2 + temp1);
-            if (post[i] == '-') {
+            if (post[i] == '-')
                 stack2.push(temp2 - temp1);
-            }
-            if (post[i] == '*') {
+            if (post[i] == '*')
                 stack2.push(temp2 * temp1);
-            }
-            if (post[i] == '/') {
+            if (post[i] == '/')
                 stack2.push(temp2 / temp1);
-            }
             }
         }
     }
