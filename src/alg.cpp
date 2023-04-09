99fefac7db7327359ea92de41c3dd9c5eda370e8
SupeRWiNdz <125024771+SupeRWiNdz@users.noreply.github.com>
2023-04-09 15:43:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,79 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+int getPrior(char c) {
+    if (c == '(') return 0;
+    if (c == ')') return 1;
+    if ((c == '+') || (c == '-')) return 2;
+    if ((c == '*') || (c == '/')) return 3;
 }
 
-int eval(std::string pref) {
-  // добавьте код
-  return 0;
+std::string infx2pstfx(std::string inf) {
+    TStack<char, 100> stack1;
+    std::string output = "";
+    for (int i = 0; i < inf.length(); i++) {
+        if (inf[i] >= '0' && inf[i] <= '9')
+        {
+            output += inf[i];
+            output += " ";
+        }
+        else if (inf[i] == '(')
+            stack1.push(inf[i]);
+        else if (inf[i] == '-' || inf[i] == '+' || inf[i] == '/' || inf[i] == '*')
+        {
+            while (stack1.isEmpty() == 0 && getPrior(inf[i]) <= getPrior(stack1.getTop()))
+            {
+                output += stack1.getTop();
+                output += " ";
+                stack1.pop();
+            }
+            stack1.push(inf[i]);
+        }
+        else if (inf[i] == ')') {
+            while (stack1.isEmpty()==0 && stack1.getTop() != '(')
+            {
+                output += stack1.getTop();
+                output += " ";
+                stack1.pop();
+            }
+            stack1.pop();
+        }
+
+    }
+    while (stack1.isEmpty()==0) {
+        output += stack1.getTop();
+        output += " ";
+        stack1.pop();
+    }
+    return output;
+}
+int eval(std::string post) {
+    TStack<int, 100> stack2;
+    for (int i = 0; i < post.length(); i++) {
+        if (post[i] == ' ') {
+            continue;
+        }
+        if (post[i] >= '0' && post[i] <= '9') {
+            stack2.push(post[i] - '0');
+        }
+        else if (post[i] == '-' || post[i] == '+' || post[i] == '/' || post[i] == '*') {
+            int temp1 = stack2.getTop();
+            stack2.pop();
+            int temp2 = stack2.getTop();
+            stack2.pop();
+            if (post[i] == '+') {
+                stack2.push(temp2 + temp1);
+            if (post[i] == '-') {
+                stack2.push(temp2 - temp1);
+            }
+            if (post[i] == '*') {
+                stack2.push(temp2 * temp1);
+            }
+            if (post[i] == '/') {
+                stack2.push(temp2 / temp1);
+            }
+            }
+        }
+    }
+    return stack2.getTop();
 }
