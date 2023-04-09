9e8b9812825c51866c0bfed4f0b6d3a5bb5933bc
SupeRWiNdz <125024771+SupeRWiNdz@users.noreply.github.com>
2023-04-09 17:20:40 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -27,12 +27,10 @@ class TStack {
         throw std::string("Empty!");
     else
         return arr[top];
-    bool isEmpty() const {
-        return top == -1;
-    }
-    bool isFull() const {
-    return top == size - 1;
-    }
-}
+    bool isEmpty() const
+        return top == -1;]
+    bool isFull() const
+      return top == size - 1;
+};
 
 #endif  // INCLUDE_TSTACK_H_
